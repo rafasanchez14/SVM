cb014b8e68162240aa8b75e959e6ecabcc2a21a2
Unknown <michele_lanza1@hotmail.com>
2019-01-16 00:42:02 -0400

    envio de datos

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,7 +20,8 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Connection();
+            // Connection();
+         
             Menu();
         }
 
@@ -32,21 +33,19 @@ namespace ConsoleApp1
             Console.WriteLine("");
             Console.WriteLine("--1 ------------------- commit");
             Console.WriteLine("--2 ------------------- update");
-            Console.WriteLine("--3 ------------------- Saber si es principal");
+
             string sOpcion = Console.ReadLine();
 
             switch (sOpcion)
             {
                 case "1":
+                    Commit();
 
                     break;
                 case "2":
                   
                     break;
-                case "3":
-                   
-                    break;
-               
+                              
             
                 default:
                     Console.Clear();
@@ -60,10 +59,13 @@ namespace ConsoleApp1
         public static void Connection()
             {
                 byte[] data = new byte[10];
-
+            /*
                 IPHostEntry iphostInfo = Dns.GetHostEntry(Dns.GetHostName());
                 IPAddress ipAdress = iphostInfo.AddressList[0];
-                IPEndPoint ipEndpoint = new IPEndPoint(ipAdress, 9595);
+                IPEndPoint ipEndpoint = new IPEndPoint(ipAdress, 9595);*/
+             IPHostEntry iphostInfo = Dns.GetHostEntry("localhost");
+            IPAddress ipAdress = iphostInfo.AddressList[0];
+            IPEndPoint ipEndpoint = new IPEndPoint(ipAdress, 11000);
 
                 Socket client = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
@@ -72,16 +74,16 @@ namespace ConsoleApp1
                 {
 
                     client.Connect(ipEndpoint);
-                    Console.WriteLine("Socket created to {0}", client.RemoteEndPoint.ToString());
+                  //  Console.WriteLine("Socket created to {0}", client.RemoteEndPoint.ToString());
+                  Console.WriteLine("Conectado");
 
-
-                    while (true)
+                    /*while (true)
                     {
 
                         string message = Console.ReadLine();
                         byte[] sendmsg = Encoding.ASCII.GetBytes(message);
                         int n = client.Send(sendmsg);
-                    }
+                    }*/
 
 
                 }
@@ -94,8 +96,47 @@ namespace ConsoleApp1
                 Console.ReadKey();
 
             }
+        public static void Commit()
+        {
 
+            try
+            {
+                // Establish the local endpoint for the socket.
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+                // Create a TCP socket.
+                Socket client = new Socket(AddressFamily.InterNetwork,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint.
+                client.Connect(ipEndPoint);
+
+                // There is a text file test.txt located in the root directory.
+                string fileName = "C:\\+test.txt";
+
+                // Send file fileName to remote device
+                Console.WriteLine("Sending {0} to the host.", fileName);
+                client.SendFile(fileName);
+
+                // Release the socket.
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+        catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("Transmission end.");
+            Console.ReadKey();
 
         }
+
+ 
+
+
+    }
+
     }
 
